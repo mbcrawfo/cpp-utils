d29f8e17e8b3ce2926d853517b3898f5fe840fea
Michael Crawford <mbcrawfo@gmail.com>
2014-07-25 10:05:22 -0400

    Setup common headers and debug options header

---
+++ utils/include/common.h
@@ -0,0 +1,76 @@
+/* The MIT License (MIT)
+*
+* Copyright (c) 2014 Michael Crawford
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*/
+
+/** \file
+ * Includes common headers that are used throughout the utilities, and sets 
+ * any platform or compiler specific options.
+ */
+
+#ifndef COMMON_H_INCLUDED__
+#define COMMON_H_INCLUDED__
+
+#include "platform.h"
+#include "debug.h"
+
+// set compiler specific options
+#if COMPILER == COMPILER_MSVC
+  #define _CRT_SECURE_NO_WARNINGS
+  #define WIN32_LEAN_AND_MEAN
+  #define NOMINMAX
+
+  #if defined(USE_STL_DEBUGGING)
+    #define _ITERATOR_DEBUG_LEVEL 1
+  #else
+    #define _ITERATOR_DEBUG_LEVEL 0
+  #endif
+
+#elif COMPILER == COMPILER_GNUCXX
+  #if defined(USE_STL_DEBUGGING)
+    #define _GLIBCXX_DEBUG
+  #else
+    #undef _GLIBCXX_DEBUG
+  #endif
+
+#elif COMPILER == COMPILER_CLANG
+  #if defined(USE_STL_DEBUGGING)
+    #error clang STL debug settings have not been added
+  #endif
+
+#elif COMPILER == COMPILER_INTEL
+  #if defined(USE_STL_DEBUGGING)
+    #error Intel STL debug settings have not been added
+  #endif
+#endif
+
+// common standard headers
+#include <cstdint>
+#include <cassert>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <type_traits>
+#include <utility>
+#include <limits>
+#include <memory>
+
+#endif
